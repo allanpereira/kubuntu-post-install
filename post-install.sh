5289b2844f867ca902efc077f926b8a62516b660
Allan Pereira <allan.pereira.cunha@gmail.com>
2023-06-19 00:06:36 -0300

    Fix desktop entries

--- post-install.sh
+++ post-install.sh
@@ -211,7 +211,8 @@ export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:
 print "Creating desktop entries..."
 for file in assets/desktop-entries/*.desktop
 do
-    envsubst < "$file" > "$HOME/Desktop/$(basename $file)"
+    envsubst < "$file" > "/usr/share/applications/$(basename $file)"
+    ln -s "/usr/share/applications/$(basename $file)" "$HOME/Desktop/$(basename $file)"
 done
 
 print "Installing Brew packages..."
